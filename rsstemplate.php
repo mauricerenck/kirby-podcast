7e5365a7b51f28833aba92572ceaa98121297ef4
Maurice Renck <work@maurice-renck.de>
2015-11-07 17:29:58 +0100

    template xml output

--- rsstemplate.php
+++ rsstemplate.php
@@ -98,7 +98,7 @@
 			<itunes:author><?php echo xml($item->author()); ?></itunes:author>
 			<itunes:subtitle><?php echo xml($item->subtitle()); ?></itunes:subtitle>
 			<itunes:summary>
-			<?php echo $item->{$textfield}()->kirbytext() ?>
+				<![CDATA[<?php echo $item->{$textfield}()->kirbytext() ?>]]>
 			</itunes:summary>
 		</item>
 		<?php endforeach ?>
