15079558fe8fd25cc9a809fad31d318868d1804e
Maurice Renck <work@maurice-renck.de>
2016-07-21 21:55:10 +0200

    updated template

--- podcastfeed.sample.template.php
+++ podcastfeed.sample.template.php
@@ -2,24 +2,13 @@
 /**
  * This file has to be copied to the site/templates directory
  * Name it podcastfeed.php
- * 
+ *
  * @author Maurice Renck <hello@maurice-renck.de>
  * @see Bastian Allgeier <bastian@getkirby.com>
  */
 
-/**
-*	These are the default value
-*	If You don't need to change them, just remove this array and 
-*	remove $options from podcast() below;
-*/
-$options = array(
-	'datefield' => 'date',
-	'textfield' => 'text',
-	'excerpt' => false,
-	'header' => true,
-	'snippet' => false
-)
-
 // Change 'episodes' to your needs, insert the parent content-folder
 // where all your episodes live in
-echo page('episodes')->children()->visible()->podcast($page, $options);
+echo page('episodes')->children()->visible()->podcast($page, array(
+	'textfield'      => 'intro'
+));
