4b285bd102578b2197451a03bc0763a8e81e2813
Maurice Renck <work@maurice-renck.de>
2017-10-03 13:14:36 +0200

    only list episodes with audio in rss feed
    This way you can mix podcast and blogposts.

--- templates/rss.php
+++ templates/rss.php
@@ -63,69 +63,70 @@
 		?>
 		<?php $episodes = $page->parent()->children()->visible()->flip()->filterBy('date', '<=', time()); ?>
 
-
 		<?php foreach($episodes as $episode): ?>
-		<item>
-			<title><?php echo xml($episode->title()); ?></title>
-			<link><?php echo xml($episode->url()); ?></link>
-			<guid isPermaLink="false"><?php echo xml($episode->id()); ?></guid>
-			<pubDate><?php echo date('r', $episode->date()); ?></pubDate>
-			<description><![CDATA[<?php echo $episode->poddescr()->kirbytext() ?>]]></description>
-			<atom:link  href="<?php echo xml($episode->url()); ?>"/>
+			<?php if($episode->hasAudio()): ?>
+				<item>
+					<title><?php echo xml($episode->title()); ?></title>
+					<link><?php echo xml($episode->url()); ?></link>
+					<guid isPermaLink="false"><?php echo xml($episode->id()); ?></guid>
+					<pubDate><?php echo date('r', $episode->date()); ?></pubDate>
+					<description><![CDATA[<?php echo $episode->poddescr()->kirbytext() ?>]]></description>
+					<atom:link  href="<?php echo xml($episode->url()); ?>"/>
 
-			<?php foreach($episode->audio() as $audio): ?>
-				<?php
-					$duration = $audio->duration();
+					<?php foreach($episode->audio() as $audio): ?>
+						<?php
+							$duration = $audio->duration();
 
-					// check if length information is already written to meta-file
-					// if not, write the information
-					if($audio->duration()->empty()) {
-						$path				= $audio->root();
-						$mixinfo			= $getID3->analyze($path);
-						$duration			= $mixinfo['playtime_string'];
-						list($mins , $secs)	= explode(':' , $duration);
+							// check if length information is already written to meta-file
+							// if not, write the information
+							if($audio->duration()->empty()) {
+								$path				= $audio->root();
+								$mixinfo			= $getID3->analyze($path);
+								$duration			= $mixinfo['playtime_string'];
+								list($mins , $secs)	= explode(':' , $duration);
 
-						$hours = 0;
-						if($mins > 60) {
-							$hours	= intval($mins / 60);
-							$mins	= $mins - $hours*60;
-						}
+								$hours = 0;
+								if($mins > 60) {
+									$hours	= intval($mins / 60);
+									$mins	= $mins - $hours*60;
+								}
 
-						$duration = sprintf("%02d:%02d:%02d" , $hours , $mins , $secs);
+								$duration = sprintf("%02d:%02d:%02d" , $hours , $mins , $secs);
 
-						// Update file info, so we don't have to determine the duration again
-						$audio->update(array(
-							'duration' => $duration
-						));
-					}
+								// Update file info, so we don't have to determine the duration again
+								$audio->update(array(
+									'duration' => $duration
+								));
+							}
 
-					$audioUrl = $episode->url().'/download/'.$audio->filename();
-				?>
-				<enclosure url="<?php echo $audioUrl ?>" length="<?php echo $audio->size() ?>" type="<?php echo $audio->mime() ?>"/>
-			<?php endforeach; ?>
-			<itunes:duration><?php echo $duration; ?></itunes:duration>
+							$audioUrl = $episode->url().'/download/'.$audio->filename();
+						?>
+						<enclosure url="<?php echo $audioUrl ?>" length="<?php echo $audio->size() ?>" type="<?php echo $audio->mime() ?>"/>
+					<?php endforeach; ?>
+					<itunes:duration><?php echo $duration; ?></itunes:duration>
 
-			<itunes:author><?php echo xml($episode->author()->or($page->itunesAuthor())); ?></itunes:author>
-			<itunes:subtitle><?php echo xml($episode->podsubtitle()); ?></itunes:subtitle>
-			<itunes:summary><?php echo xml($episode->podsubtitle()); ?></itunes:summary>
+					<itunes:author><?php echo xml($episode->author()->or($page->itunesAuthor())); ?></itunes:author>
+					<itunes:subtitle><?php echo xml($episode->podsubtitle()); ?></itunes:subtitle>
+					<itunes:summary><?php echo xml($episode->podsubtitle()); ?></itunes:summary>
 
-			<?php if($episode->podseason()->isNotEmpty()): ?><itunes:season><?php echo xml($episode->podseason()); ?></itunes:season><?php endif; ?>
-			<?php if($episode->podepisode()->isNotEmpty()): ?><itunes:episode><?php echo xml($episode->podepisode()); ?></itunes:episode><?php endif; ?>
-			<?php if($episode->podtitle()->isNotEmpty()): ?><itunes:title><?php echo xml($episode->podtitle()); ?></itunes:title><?php endif; ?>
-			<?php if($episode->episodeType()->isNotEmpty()): ?><itunes:episodeType><?php echo xml($episode->episodeType()); ?></itunes:episodeType><?php endif; ?>
-			<content:encoded><![CDATA[
-				<?php echo $episode->poddescr()->kirbytext() ?>
-			]]></content:encoded>
+					<?php if($episode->podseason()->isNotEmpty()): ?><itunes:season><?php echo xml($episode->podseason()); ?></itunes:season><?php endif; ?>
+					<?php if($episode->podepisode()->isNotEmpty()): ?><itunes:episode><?php echo xml($episode->podepisode()); ?></itunes:episode><?php endif; ?>
+					<?php if($episode->podtitle()->isNotEmpty()): ?><itunes:title><?php echo xml($episode->podtitle()); ?></itunes:title><?php endif; ?>
+					<?php if($episode->episodeType()->isNotEmpty()): ?><itunes:episodeType><?php echo xml($episode->episodeType()); ?></itunes:episodeType><?php endif; ?>
+					<content:encoded><![CDATA[
+						<?php echo $episode->poddescr()->kirbytext() ?>
+					]]></content:encoded>
 
-			<?php if($episode->chapters()->isNotEmpty()): ?>
-				<!-- specify chapter information -->
-				<psc:chapters version="1.2" xmlns:psc="http://podlove.org/simple-chapters">
-				<?php foreach($episode->chapters()->yaml() as $chapter): ?>
-					<psc:chapter start="<?php echo $chapter['timestamp']; ?>" title="<?php echo xml($chapter['title']); ?>" <?php if($chapter['url']): ?>href="<?php echo xml($chapter['url']); ?>"<?php endif; ?> />
-				<?php endforeach; ?>
-				</psc:chapters>
+					<?php if($episode->chapters()->isNotEmpty()): ?>
+						<!-- specify chapter information -->
+						<psc:chapters version="1.2" xmlns:psc="http://podlove.org/simple-chapters">
+						<?php foreach($episode->chapters()->yaml() as $chapter): ?>
+							<psc:chapter start="<?php echo $chapter['timestamp']; ?>" title="<?php echo xml($chapter['title']); ?>" <?php if($chapter['url']): ?>href="<?php echo xml($chapter['url']); ?>"<?php endif; ?> />
+						<?php endforeach; ?>
+						</psc:chapters>
+					<?php endif; ?>
+				</item>
 			<?php endif; ?>
-		</item>
 		<?php endforeach ?>
 	</channel>
 </rss>
